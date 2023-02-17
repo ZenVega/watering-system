49c84d8a780e189d63ebdfbdde7df34609fc6bf0
Urs Schmidt <urs.schmidt@ts.berlin>
2023-02-17 17:58:47 +0100

    🤏 Get moisture average

--- src/moisture_sensor.cpp
+++ src/moisture_sensor.cpp
@@ -1,10 +1,28 @@
 #include <Arduino.h>
+#include <iostream>
+#include <vector>
+using namespace std;
 
 int readSensor(int GPIO_pin)
 {
+  int counter = 0;
+  int average = 0;
   int maxMoisture = 1000;
-  int minMoisture = 2650;
-  int read_value = analogRead(GPIO_pin); // read the analog value from sensor
-  int moisture_Percentage = map(read_value, minMoisture, maxMoisture, 0, 100);
+  int minMoisture = 3000;
+  vector<int> measurements;
+
+  while (counter < 5)
+  {
+    int read_value = analogRead(GPIO_pin); // read the analog value from sensor
+    measurements.push_back(read_value);
+    counter++;
+  }
+
+  for (const int &i : measurements)
+  {
+    average += i;
+  }
+  average = average / measurements.size();
+  int moisture_Percentage = map(average, minMoisture, maxMoisture, 0, 100);
   return moisture_Percentage;
 };
